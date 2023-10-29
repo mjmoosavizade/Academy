f352afe10975ba086e543c1e728a59864ddfe354
mjmoosavizade <mjmoosavizade@gmail.com>
2023-10-29 20:54:13 +0330

    Added Navbar Image

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Link from 'next/link';
 import {FaYoutube, FaGithub, FaTwitter, FaLaptop} from 'react-icons/fa'
 import {useScrollPosition} from "@/app/hooks/useScrollPosition";
+import Image from "next/image";
 
 export function Navbar() {
     function classNames(...classes: any) {
@@ -14,19 +15,13 @@ export function Navbar() {
         <nav className={classNames(
             scrollPosition > 0 ? 'shadow backdrop-blur-lg' : 'shadow-none backdrop-blur-none',
             'bg-transparent p-4 sticky top-0 drop-shadow-xl z-10',
-        )} >
-            <div className="prose prose-xl mx-auto flex justify-between flex-col sm:flex-row">
-                <h1 className="text-2xl font-bold text-white grid place-content-center mb-2 md:mb-0">
-                    <Link href="/" className="text-white/90 no-underline hover:text-white">
-                        WellBeauty Academy
-                    </Link>
-                </h1>
-                <div className="flex flex-row justify-center sm:justify-evenly align-middle gap-4 text-white text-4xl lg:text-5xl">
+        )}>
+            <div className="prose prose-xl mx-auto flex justify-between flex-col sm:flex-row-reverse items-end">
 
-                    <Link className="text-white/50 hover:text-white" href="https://twitter.com/">
-                        <FaTwitter size={30} />
-                    </Link>
-                </div>
+                <Link href="/" className="no-underline w-fit">
+                    <Image src="/images/WellBeauty_logo.png" alt="Logo" width={113} height={56} className="m-0"/>
+                </Link>
+
             </div>
         </nav>
     );
