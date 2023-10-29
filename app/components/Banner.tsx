a30b18fba1cfc5f1f51aba6e10a4db3cff99b263
mjmoosavizade <mjmoosavizade@gmail.com>
2023-10-29 20:15:57 +0330

    Changed Banner image

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -3,14 +3,14 @@ import Image from 'next/image'
 
 export function Banner() {
     return (
-        <section className="w-full mx-auto top bottom-28 sm:lg:bottom-20 md:bottom-20 lg:bottom-16 relative h-screen">
+        <section className="w-full mx-auto top bottom-28 sm:bottom-24 md:bottom-24 lg:bottom-24 relative h-screen">
             <Image
                 className="w-full h-full"
-                src="/images/bis-biswas-passing-by.jpg"
+                src="/images/Banner.png"
                 sizes="100vw"
                 alt="Profile"
                 fill
-                style={{objectFit: "cover"}}
+                style={{objectFit: "cover", objectPosition:"left"}}
                 priority={true}/>
         </section>
     );
