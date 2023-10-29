f0ec85c1a22994b3c4f8315dfec7fcfa3b5b55e9
mjmoosavizade <mjmoosavizade@gmail.com>
2023-10-29 20:53:37 +0330

    Added font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import {Navbar} from "@/app/components/Navbar";
-import {Banner} from "@/app/components/Banner";
 import React from "react";
 import Footer from "@/app/components/Footer";
+import localFont from 'next/font/local'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,14 +11,30 @@ export const metadata = {
   title: "دوره های آموزشی ول‌بیوتی",
   description: 'دوره ها ',
 }
-
+const IranSans = localFont({
+    src: [
+        {
+            path: '../public/fonts/IRANSansRegular.ttf',
+            weight: '400'
+        },
+        {
+            path: '../public/fonts/IRANSansUltraLight.ttf',
+            weight: '200'
+        },
+        {
+            path: '../public/fonts/IRANSansMedium.ttf',
+            weight: '500'
+        }
+    ],
+    variable: '--font-IRANSans'
+})
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${IranSans.variable} font-sans`}>
       <body className={inter.className}>
       <Navbar />
 
